c977923fd299f6721deeb0eb7d57e965b0c68821
Aditya Sharma <125301893+Adityasva26@users.noreply.github.com>
2023-06-26 15:39:45 +0530

    Update next.config.js
    chages

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
      URL: 'http://192.168.1.36:4000/api/futurePedia/',
-     NEXT_PUBLIC_BASE_URL:"http://localhost:3000"
+     NEXT_PUBLIC_BASE_URL:"http://findup.ai"
   }
 }
 
