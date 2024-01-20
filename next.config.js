dcd92b036ff1723d963a908f740aca50627f8f94
Aditya Sharma <aditya.sva26@gmail.com>
2024-01-20 11:25:02 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -3,9 +3,22 @@ const nextConfig = {
   target: 'server', 
   reactStrictMode: true,
   env: {
-     URL: 'https://api.findup.ai/api/futurePedia/',
-     NEXT_PUBLIC_BASE_URL:"http://findup.ai"
-  }
-}
+     URL: 'http://192.168.56.1:4000/api/futurePedia/',
+     NEXT_PUBLIC_BASE_URL: "http://findup.ai"
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin", // "same-origin-allow-popups"
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
